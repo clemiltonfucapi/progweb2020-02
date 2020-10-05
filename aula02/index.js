6f4c48da3efd4cb821dc759a9653ad35de748bde
clemiltonfucapi <clemilton.fucapi@gmail.com>
2020-10-05 18:46:35 -0400

    aula sabado 03/10

---
+++ aula02/index.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('input[type="file"]')
+
+
+input.addEventListener('change',e =>{
+     console.log(input.files)
+     const reader = new FileReader();
+        reader.onload = function(){
+            let img = new Image();
+            img.src = reader.result;
+            document.body.appendChild(img)
+        
+        }
+    reader.readAsDataURL(input.files[0])
+
+})
